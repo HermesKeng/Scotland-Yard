45f29202919bc8b3a8e11b77726a26d3bf1a3683
Keng <kaining1101@gmail.com>
2017-02-05 19:16:12 +0800

    新增檔案-Client/Server
    1.本次遊戲架構主軸是五個人玩的遊戲，而這五個人分別是1位mr.X 和4位警察
    2.我們用一個Server負責控制整個流程，並且將所有的資料透過Server並一一的進行broadcast
    3.在Server端一開啟程式會看到主畫面，是一個簡單的訊息式窗，按下start connect後，會產生除了main thread
    外的五個Thread，由於我們在Thread端不能直接更改UI元件，也因此我們用了delegate的方式，間接的在Thread中記錄並更改UI的元件
    
    4.Client端的部分，我們必須輸入IP，輸入IP後便可以和Server進行連結，若Server沒有連結到，則我們便會產生錯誤訊息"connection
    fail"

---
+++ Game Server/Game Server/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Windows.Forms;
+
+namespace Game_Server
+{
+    public partial class Form1 : Form
+    {
+        Server server = new Server();
+        TcpClient[] client=new TcpClient[4];
+        Thread[] thread=new Thread[4];
+        ParameterizedThreadStart ts;
+        private delegate void callbUI(String message,Control ctl);
+        private void UpdateUI(string value, Control ctl)
+        {
+            if (this.InvokeRequired)
+            {
+                callbUI cb = new callbUI(UpdateUI);
+                this.Invoke(cb,value,ctl);
+            }
+            else
+            {
+                ctl.Text += value;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            server.Start(this);
+            for (int i = 0; i < 4; i++)
+            {
+                ts = new ParameterizedThreadStart(Create_Connection);
+                thread[i] = new Thread(ts);
+                thread[i].Start(i);
+            }
+            for (int i = 0; i < 4; i++)
+            {
+                while (!thread[i].IsAlive) ;
+            }
+
+        }
+        public void Create_Connection(object str){
+
+            string msg;
+            int i = Int32.Parse(str.ToString());
+            msg = "Thread" + i + "start\n";
+            UpdateUI(msg, infobox);
+            client[i]=server.Wait_Client(this, client[i]);
+            msg = "玩家" + (i + 1) + "成功連線\n" + "還需等待 " + (3 - i) + " 名玩家\n";
+            UpdateUI(msg, infobox);
+        }
+
+    }
+}
